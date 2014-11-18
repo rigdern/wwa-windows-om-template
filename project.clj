6cc0296a9929999610903177e1c5de3a52fe84ae
Adam Comella <adcom@microsoft.com>
2014-11-18 07:07:18 -0800

    Initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject wwa-windows-om/lein-template "0.1.0"
+  :description "A minimal template for building Windows Store Apps with ClojureScript and Om."
+  :url "https://github.com/rigdern/wwa-windows-om-template"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url "https://github.com/rigdern/wwa-windows-om-template"}
+  :eval-in-leiningen true)
---
+++ src/leiningen/new/wwa_windows_om/my-project/project.clj
@@ -0,0 +1,21 @@
+(defproject {{name}} "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2280"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [om "0.7.0"]]
+
+  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
+
+  :source-paths ["src"]
+
+  :cljsbuild {
+    :builds [{:id "{{name}}"
+              :source-paths ["src"]
+              :compiler {
+                :output-to "{{sanitized}}.js"
+                :output-dir "out"
+                :optimizations :none
+                :source-map true}}]})
